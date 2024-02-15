7faae615613faf64740e887eed18fd5e88e89037
Kyle Avedaña <avedanakyledavid.it@gmail.com>
2024-02-15 18:14:14 +0800

    assert that the page begin with a $1000 USD balance

--- frontend/cypress/e2e/app.cy.ts
+++ frontend/cypress/e2e/app.cy.ts
@@ -1,5 +1,9 @@
 describe("onLoad", () => {
   it("should begin with a $1000 USD balance", () => {
     cy.visit("http://localhost:5173");
+    cy.contains("div", "USD Balance: $").should((element) => {
+      const textContent = element.text().trim();
+      expect(textContent).to.equal("USD Balance: $1000");
+    });
   });
 });
