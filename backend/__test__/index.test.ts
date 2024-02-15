906a5a4aa00ed3f971cb6d5b42ab75d3b723d9a0
Kyle Avedaña <avedanakyledavid.it@gmail.com>
2024-02-16 01:51:19 +0800

    test that `inventory.<coinId>.amountOwned` correctly reflects inventory following a `purchase-coin` execution

--- backend/__test__/index.test.ts
+++ backend/__test__/index.test.ts
@@ -1,17 +1,19 @@
 import request from "superwstest";
 import { Inventory, Coin } from "../src";
 
+const DOMAIN = "localhost:3100";
+
 describe("testing the endpoints", () => {
   afterEach(() => request.closeAll());
 
   it("should output successful response payload from purchase-coin after a buy order is placed", async () => {
-    let response = await request("http://localhost:3100").get("/get-inventory");
+    let response = await request(`http://${DOMAIN}`).get("/get-inventory");
     const inventory: Inventory[] = response.body;
 
     const { coinId } = inventory[0];
     const amount = 3;
 
-    response = await request("http://localhost:3100")
+    response = await request(`http://${DOMAIN}`)
       .post("/purchase-coin")
       .send({ coinId, amount });
 
@@ -48,6 +50,34 @@ describe("testing the websocket", () => {
         const coinB = result.coins.find((coin) => coin.name === "CoinB");
 
         expect(coinB?.price).toEqual(101);
+      });
+  }, 10000);
+
+  it("test that `inventory.<coinId>.amountOwned` correctly reflects inventory following a `purchase-coin` execution", async () => {
+    await request("ws://localhost:3100")
+      .ws("/")
+      .expectJson()
+      .exec(async () => {
+        let response = await request(`http://${DOMAIN}`).get("/get-inventory");
+        const inventory: Inventory[] = response.body;
+
+        const { coinId } = inventory[1];
+        const amount = 3;
+
+        response = await request(`http://${DOMAIN}`)
+          .post("/purchase-coin")
+          .send({ coinId, amount });
+      })
+      .wait(5000)
+      .expectJson((data) => {
+        const result: {
+          coins: Coin[];
+          inventory: Inventory[];
+          time: number;
+        } = data;
+        console.log(result);
+        const amountOwned = result.inventory[1].amountOwned;
+        expect(amountOwned).toEqual(3);
       });
   }, 10000);
 });
