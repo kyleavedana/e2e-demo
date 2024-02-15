dda5b71f9772206093360410ade8bf96466b71c8
Kyle Avedaña <avedanakyledavid.it@gmail.com>
2024-02-16 02:12:58 +0800

    optimize code

--- backend/__test__/index.test.ts
+++ backend/__test__/index.test.ts
@@ -3,20 +3,25 @@ import { Inventory, Coin } from "../src";
 
 const DOMAIN = "localhost:3100";
 
+const purchaseCoin = async () => {
+  let response = await request(`http://${DOMAIN}`).get("/get-inventory");
+  const inventory: Inventory[] = response.body;
+
+  const { coinId } = inventory[1];
+  const amount = 3;
+
+  response = await request(`http://${DOMAIN}`)
+    .post("/purchase-coin")
+    .send({ coinId, amount });
+
+  return response;
+};
+
 describe("testing the endpoints", () => {
   afterEach(() => request.closeAll());
 
   it("should output successful response payload from purchase-coin after a buy order is placed", async () => {
-    let response = await request(`http://${DOMAIN}`).get("/get-inventory");
-    const inventory: Inventory[] = response.body;
-
-    const { coinId } = inventory[0];
-    const amount = 3;
-
-    response = await request(`http://${DOMAIN}`)
-      .post("/purchase-coin")
-      .send({ coinId, amount });
-
+    let response = await purchaseCoin();
     const status = response.body.success;
     expect(status).toEqual(true);
   });
@@ -25,56 +30,38 @@ describe("testing the endpoints", () => {
 describe("testing the websocket", () => {
   afterEach(() => request.closeAll());
 
+  interface wsResult {
+    coins: Coin[];
+    inventory: Inventory[];
+    time: number;
+  }
+
   it("test that CoinB increments by one dollar with each message over a period of time", async () => {
-    await request("ws://localhost:3100")
+    await request(`ws://${DOMAIN}`)
       .ws("/")
       .expectJson((data) => {
-        const result: {
-          coins: Coin[];
-          inventory: Inventory[];
-          time: number;
-        } = data;
-
+        const result: wsResult = data;
         const coinB = result.coins.find((coin) => coin.name === "CoinB");
-
         expect(coinB?.price).toEqual(100);
       })
       .wait(5000)
       .expectJson((data) => {
-        const result: {
-          coins: Coin[];
-          inventory: Inventory[];
-          time: number;
-        } = data;
-
+        const result: wsResult = data;
         const coinB = result.coins.find((coin) => coin.name === "CoinB");
-
         expect(coinB?.price).toEqual(101);
       });
   }, 10000);
 
   it("test that `inventory.<coinId>.amountOwned` correctly reflects inventory following a `purchase-coin` execution", async () => {
-    await request("ws://localhost:3100")
+    await request(`ws://${DOMAIN}`)
       .ws("/")
       .expectJson()
       .exec(async () => {
-        let response = await request(`http://${DOMAIN}`).get("/get-inventory");
-        const inventory: Inventory[] = response.body;
-
-        const { coinId } = inventory[1];
-        const amount = 3;
-
-        await request(`http://${DOMAIN}`)
-          .post("/purchase-coin")
-          .send({ coinId, amount });
+        await purchaseCoin();
       })
       .wait(5000)
       .expectJson((data) => {
-        const result: {
-          coins: Coin[];
-          inventory: Inventory[];
-          time: number;
-        } = data;
+        const result: wsResult = data;
         const amountOwned = result.inventory[1].amountOwned;
         expect(amountOwned).toEqual(3);
       });
